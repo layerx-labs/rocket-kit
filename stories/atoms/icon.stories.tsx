fef0bfc8bada34e8a4dc964087325bf992fa5f83
Henrique Macedo <mail@henriquemacedo.pt>
2021-01-19 19:41:02 +0000

    Add icon component

---
+++ stories/atoms/icon.stories.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Icon } from '../../src';
+import icons from '../../src/ions/icons';
+
+export default {
+  title: 'Atoms/Icon',
+  component: Icon,
+  argTypes: {
+    fill: { control: 'color' },
+    icon: {
+      control: {
+        type: 'select',
+        options: Object.keys(icons),
+      },
+    },
+  },
+};
+
+export const IconComponent = (args) => (
+  <div style={{ width: '50px', height: '50px' }}>
+    <Icon {...args} />
+  </div>
+);
+
+IconComponent.args = {
+  fill: '#000000',
+  icon: '20px',
+};
