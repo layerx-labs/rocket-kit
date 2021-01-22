74c5ef867a3a7ef40ddf8b60a28fe6236eb1e3e9
Henrique Macedo <mail@henriquemacedo.pt>
2021-01-22 17:46:15 +0000

    Remove TAIKAI logo

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 // Test
 export { Thing } from './thing';
 
-// Ions
-export { default as TaikaiLogo } from './ions/taikai-logo';
-
 // Atoms
 export { default as Avatar } from './atoms/avatar';
 export { default as Button } from './atoms/button';
