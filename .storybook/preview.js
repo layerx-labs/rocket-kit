97971738db77f34941a9e08dde1d97d26ff63462
Henrique Macedo <mail@henriquemacedo.pt>
2021-01-24 00:02:12 +0000

    Add global styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,18 @@
-// https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
+import React from 'react';
+import GlobalStyles from '../src/globalStyles';
+import { addDecorator } from '@storybook/react';
+
 export const parameters = {
-  // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
   actions: { argTypesRegex: '^on.*' },
 };
+
+export const withGlobalStyles = storyFn => {
+  return (
+    <React.Fragment>
+      <GlobalStyles />
+      {storyFn()}
+    </React.Fragment>
+  );
+};
+
+addDecorator(withGlobalStyles);
---
+++ src/globalStyles.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+import { fontWeigth } from './ions/variables';
+
+const { regular } = fontWeigth;
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    font-family: 'Inter', Verdana, Arial, Helvetica, sans-serif;
+    font-size: 1rem;
+    font-weight: ${regular};
+    color: var(--default, hsl(0, 0%, 16%))
+  }
+`;
+
+export default GlobalStyle;
