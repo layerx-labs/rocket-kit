c84040fa5f505d1cca49278022957e2b89802f41
Henrique Macedo <mail@henriquemacedo.pt>
2021-03-25 10:14:29 +0000

    Bug fix

--- src/molecules/number-input-spinner/index.tsx
+++ src/molecules/number-input-spinner/index.tsx
@@ -37,7 +37,7 @@ const NumberInputSpinner = (props: NumberInputSpinnerProps) => {
     if (onChange) {
       onChange(number);
     }
-  }, [number, onChange]);
+  }, [number]);
 
   return (
     <Styles.Wrapper>
