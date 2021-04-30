6aff43c8ea070ed006c1262c16959bdccfead0ae
Henrique Macedo <henrique@hey.com>
2021-04-30 17:02:43 +0100

    Add checkbox button component

---
+++ src/atoms/checkbox-button/stories/checkbox-button.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import CheckboxButton, { CheckboxButtonProps } from '..';
+
+export default {
+  title: 'Design System/Atoms/Checkbox',
+  component: CheckboxButton,
+  argTypes: { onChange: { action: () => {} } },
+};
+
+export const CheckboxButtonComponent = (args: CheckboxButtonProps) => (
+  <CheckboxButton {...args} />
+);
+
+CheckboxButtonComponent.storyName = 'Toggle Button';
+CheckboxButtonComponent.args = {
+  label: 'Value for Real',
+  value: 'option_1',
+  checked: false,
+  className: 'checkbox-button',
+  disabled: false,
+  required: false,
+};
