cc40ec04001101a0343e6a0e97be5f8ce64c9d7d
Henrique Macedo <henrique@hey.com>
2021-05-24 10:40:20 +0100

    Add react-select for new select-interactive atom

---
+++ src/atoms/select-interactive/index.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import Select, { components } from 'react-select';
+
+import Icon from '../icon';
+import ErrorField from '../error-field';
+import * as Styles from './styles';
+
+interface OptionProps {
+  data: {
+    value: string;
+    label: string;
+    icon: string;
+    customImage: React.ReactNode;
+  };
+}
+
+const SelectInteractive = (props: any) => {
+  const {
+    name,
+    multi = false,
+    search = true,
+    placeholder,
+    options,
+    value,
+    clear = true,
+    onChange = () => {},
+    onInputChange = () => {},
+    error,
+    disabled = false,
+    dataTestId,
+  } = props;
+
+  const { Option } = components;
+
+  const CustomSelectOption = (props: OptionProps, commonProps: any) => (
+    <Option {...props} {...commonProps}>
+      {props.data.icon ? (
+        <Icon icon={props.data.icon} />
+      ) : props.data.customImage ? (
+        props.data.customImage
+      ) : null}
+      {props.data.label}
+    </Option>
+  );
+
+  const CustomSelectValue = (props: OptionProps) => (
+    <div>
+      {props.data.icon ? (
+        <Icon icon={props.data.icon} />
+      ) : props.data.customImage ? (
+        props.data.customImage
+      ) : null}
+      {props.data.label}
+    </div>
+  );
+
+  return (
+    <Styles.SelectWrapper data-testid={dataTestId}>
+      <Select
+        className="select-interactive"
+        classNamePrefix="select"
+        name={name}
+        isMulti={multi}
+        isSearchable={search}
+        isDisabled={disabled}
+        placeholder={placeholder}
+        options={options}
+        value={value}
+        isClearable={clear}
+        onChange={onChange}
+        defaultMenuIsOpen={false}
+        onInputChange={onInputChange}
+        components={{
+          Option: CustomSelectOption,
+          SingleValue: CustomSelectValue,
+        }}
+      />
+      {error ? <ErrorField error={error} /> : null}
+    </Styles.SelectWrapper>
+  );
+};
+
+export default SelectInteractive;
---
+++ src/atoms/select-interactive/styles.tsx
@@ -0,0 +1,120 @@
+import styled from 'styled-components';
+import { rem, lighten } from 'polished';
+import { colors } from '../../ions/variables';
+
+const { normal, light, info, purple } = colors;
+
+export const SelectWrapper = styled.div`
+  .select {
+    &-interactive {
+      z-index: 3;
+    }
+
+    &__control {
+      border-color: ${info};
+      min-height: ${rem('50px')};
+      transition-duration: 0.3s;
+
+      &:hover {
+        border-color: ${purple};
+      }
+
+      &--is-focused {
+        border-color: ${purple};
+        box-shadow: none;
+      }
+    }
+
+    &__value-container {
+      align-items: center;
+      padding: 0 ${rem('15px')};
+    }
+
+    &__placeholder {
+      color: ${info};
+    }
+
+    &__input {
+      color: ${normal};
+    }
+
+    &__single-value {
+      color: ${normal};
+    }
+
+    &__indicator {
+      padding: 0 ${rem('15px')};
+
+      &:hover {
+        color: inherit;
+      }
+
+      svg {
+        width: ${rem('22px')};
+        height: ${rem('22px')};
+
+        path {
+          fill: hsl(0, 0%, 85%);
+        }
+      }
+
+      &-separator {
+        background-color: ${info};
+      }
+    }
+
+    &__option {
+      transition-duration: 0.3s;
+
+      &:hover {
+        background-color: ${purple};
+        color: ${light};
+      }
+
+      &--is-focused {
+        background-color: ${purple};
+        color: ${light};
+      }
+
+      &--is-selected {
+        background-color: ${lighten(0.4, purple)};
+        color: ${normal};
+      }
+    }
+  }
+
+  .select__value-container > div,
+  .select__option {
+    display: flex;
+    align-items: center;
+  }
+
+  .select__value-container.select__value-container--is-multi > div {
+    align-items: stretch;
+  }
+
+  .select__value-container,
+  .select__option {
+    svg,
+    img {
+      margin: 0px ${rem('5px')} 0 0;
+      width: ${rem('20px')};
+      height: auto;
+      transition-duration: 0.3s;
+    }
+  }
+
+  .select__option--is-selected {
+    svg {
+      fill: ${normal};
+      transition-duration: 0.3s;
+    }
+  }
+
+  .select__option--is-focused {
+    svg {
+      fill: ${light};
+      transition-duration: 0.3s;
+    }
+  }
+`;
