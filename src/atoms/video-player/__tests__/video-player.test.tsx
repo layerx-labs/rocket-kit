78d3a2c21ace4b47d9e75ab3f4d521c0d5d9d1c9
Henrique Macedo <henrique@hey.com>
2021-09-27 18:39:38 +0100

    Add tests

---
+++ src/atoms/video-player/__tests__/video-player.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import 'jest-styled-components';
+import { VideoPlayer } from '../../..';
+
+describe('VideoPlayer', () => {
+  it('renders', () => {
+    const { asFragment } = render(
+      <VideoPlayer video="https://www.youtube.com/watch?v=E71ffwc13y4" />
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
