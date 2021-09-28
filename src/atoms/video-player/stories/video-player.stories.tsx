ef68be7f280131d582da47ddeb750a6bf113e170
Henrique Macedo <mail@henriquemacedo.pt>
2021-09-28 09:38:51 +0100

    [TAIK-2310] - Add new video player component (#65)
    * Add react-player lib
    
    * Add video-player component
    
    * Add tests

---
+++ src/atoms/video-player/stories/video-player.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import VideoPlayer, { VideoPlayerProps } from '..';
+
+export default {
+  title: 'Design System/Atoms/Video Player',
+  component: VideoPlayer,
+  argTypes: {
+    onEnded: { action: 'video ended' },
+  },
+};
+
+export const VideoPlayerComponent = (args: VideoPlayerProps) => {
+  return <VideoPlayer {...args} />;
+};
+
+VideoPlayerComponent.storyName = 'Video Player';
+
+VideoPlayerComponent.args = {
+  video: 'https://www.youtube.com/watch?v=E71ffwc13y4',
+  playing: false,
+  loop: false,
+  controls: true,
+  width: '100%',
+  height: '100%',
+};
