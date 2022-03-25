52a786b3dba6c8f384076689e7d7164914fe9df3
Henrique Macedo <henrique@hey.com>
2022-03-25 14:36:52 +0000

    Add integration with Figma

--- src/molecules/pagination-control/stories/pagination-control.stories.tsx
+++ src/molecules/pagination-control/stories/pagination-control.stories.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { withDesign } from 'storybook-addon-designs';
 import PaginationControl, { PageControlProps } from '..';
 
 export default {
   title: 'Design System/Molecules/PaginationControl',
   component: PaginationControl,
+  decorators: [withDesign],
 };
 
 export const PaginationControlComponent = (args: PageControlProps) => (
@@ -15,4 +17,11 @@ PaginationControlComponent.args = {
   page: 0,
   pageCount: 10,
   onPageChange: () => {},
+};
+
+PaginationControlComponent.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/2aV5JcANU1uWQntLdMSTti/TAIKAI-Design-System?node-id=2%3A2',
+  },
 };
