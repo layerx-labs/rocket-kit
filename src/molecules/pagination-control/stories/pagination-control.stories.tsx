825fded0e74e971cc8fda5480650dd4f0292d85c
Henrique Macedo <henrique@hey.com>
2022-03-29 12:51:30 +0100

    Update PaginationControl component with new design and light and dark mode support

--- src/molecules/pagination-control/stories/pagination-control.stories.tsx
+++ src/molecules/pagination-control/stories/pagination-control.stories.tsx
@@ -8,20 +8,40 @@ export default {
   decorators: [withDesign],
 };
 
-export const PaginationControlComponent = (args: PageControlProps) => (
+export const PaginationControlLight = (args: PageControlProps) => (
   <PaginationControl {...args} />
 );
 
-PaginationControlComponent.storyName = 'PaginationControl';
-PaginationControlComponent.args = {
+PaginationControlLight.storyName = 'Light';
+PaginationControlLight.args = {
   page: 0,
   pageCount: 10,
   onPageChange: () => {},
 };
 
-PaginationControlComponent.parameters = {
+PaginationControlLight.parameters = {
   design: {
     type: 'figma',
     url: 'https://www.figma.com/file/2aV5JcANU1uWQntLdMSTti/TAIKAI-Design-System?node-id=2%3A2',
+  },
+};
+
+export const PaginationControlDark = (args: PageControlProps) => (
+  <PaginationControl {...args} />
+);
+
+PaginationControlDark.storyName = 'Dark';
+PaginationControlDark.args = {
+  dark: true,
+  page: 0,
+  pageCount: 10,
+  onPageChange: () => {},
+};
+
+PaginationControlDark.parameters = {
+  backgrounds: { default: 'dark' },
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/2aV5JcANU1uWQntLdMSTti/TAIKAI-Design-System?node-id=36%3A49',
   },
 };
