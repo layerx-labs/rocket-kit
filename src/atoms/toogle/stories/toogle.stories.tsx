befe802df90ba5b89538aac0cc228c8b0220d306
Rtandrew Paul <anderson_rodax@hotmail.com>
2022-04-19 19:36:43 +0100

    Update src/atoms/toogle/stories/toogle.stories.tsx
    Co-authored-by: Henrique Macedo <henrique@hey.com>

--- src/atoms/toogle/stories/toogle.stories.tsx
+++ src/atoms/toogle/stories/toogle.stories.tsx
@@ -8,4 +8,12 @@ export default {
 };
 
 export const ToogleComponent = (args: ToggleProps) => <Toggle {...args} />;
+
 ToogleComponent.storyName = 'Toggle';
+ToogleComponent.args = {
+  checked: true,
+  isLabelVisible: true,
+  labelLeft: 'Off',
+  labelRight: 'On',
+  disabled: false,
+};
