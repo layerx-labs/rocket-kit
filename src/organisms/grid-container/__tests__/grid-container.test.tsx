07b1573c39f0c0c83b503d0a9c71c9fd2920233b
Henrique Macedo <henrique@hey.com>
2022-05-09 11:04:17 +0100

    Typo

--- src/organisms/grid-container/__tests__/grid-container.test.tsx
+++ src/organisms/grid-container/__tests__/grid-container.test.tsx
@@ -8,9 +8,7 @@ describe('GridContainer', () => {
     const { asFragment } = render(
       <GridContainer>
         <GridRow>
-          <GridCol>
-            Col 1ldhf lasdjfh lkjasdhflkasd fklahds fkljahs dklfjh asdklfh asl
-          </GridCol>
+          <GridCol>Col 1</GridCol>
           <GridCol>Col 2</GridCol>
           <GridCol size={2}>Col 3</GridCol>
         </GridRow>
--- src/organisms/grid-container/stories/grid-container.stories.tsx
+++ src/organisms/grid-container/stories/grid-container.stories.tsx
@@ -12,9 +12,7 @@ export const GridContainerComponent = () => {
   return (
     <GridContainer>
       <GridRow>
-        <GridCol>
-          Col 1ldhf lasdjfh lkjasdhflkasd fklahds fkljahs dklfjh asdklfh asl
-        </GridCol>
+        <GridCol>Col 1</GridCol>
         <GridCol>Col 2</GridCol>
         <GridCol size={2}>Col 3</GridCol>
       </GridRow>
