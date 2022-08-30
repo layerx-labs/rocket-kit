48bfc99a0be679316acf25e54b018ebf5f0963ec
Henrique Macedo <henrique@taikai.network>
2022-08-30 14:33:25 +0100

    Add new props on Button component

--- src/atoms/button/stories/button.stories.tsx
+++ src/atoms/button/stories/button.stories.tsx
@@ -1,44 +1,40 @@
 import React from 'react';
 import Button, { ButtonProps } from '..';
 import icons from '../../../ions/icons';
+import { colors } from '../../../ions/variables';
 
 export default {
   title: 'Components/Atoms/Button',
   component: Button,
   argTypes: {
-    color: {
-      control: {
-        type: 'select',
-        options: [
-          'green',
-          'orange',
-          'red',
-          'grey',
-          'purple',
-          'blue',
-          'white',
-          'dark',
-          'magic',
-          'pulse',
-        ],
-      },
-    },
     variant: {
       control: {
         type: 'inline-radio',
         options: ['solid', 'outline', 'text'],
       },
     },
-    icon: {
+    color: {
       control: {
         type: 'select',
-        options: Object.keys(icons),
+        options: Object.keys(colors),
+      },
+    },
+    txtColor: {
+      control: {
+        type: 'select',
+        options: Object.keys(colors),
       },
     },
     iconPosition: {
       control: {
-        type: 'select',
+        type: 'inline-radio',
         options: ['left', 'right'],
+      },
+    },
+    icon: {
+      control: {
+        type: 'select',
+        options: ['', ...Object.keys(icons)],
       },
     },
   },
@@ -48,15 +44,16 @@ export const ButtonComponent = (args: ButtonProps) => <Button {...args} />;
 
 ButtonComponent.storyName = 'Action';
 ButtonComponent.args = {
-  value: 'Dummie',
   variant: 'solid',
-  color: 'green',
-  circle: false,
+  rounded: false,
+  color: 'purple500',
+  txtColor: 'white',
   className: 'button',
-  querySelector: '.button',
-  ariaLabel: 'Dummie Button',
-  disabled: false,
+  value: 'Dummie',
+  iconPosition: 'right',
+  icon: '',
   loading: false,
-  icon: 'rocket',
-  iconPosition: 'left',
+  ariaLabel: 'Dummie Button',
+  ariaHidden: false,
+  disabled: false,
 };
