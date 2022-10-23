9a8b00fb69cb0a8a701300e3769a3a79be9e8895
Stanislav Pintjuk <stani@stani.se>
2022-10-23 19:55:43 +0200

    added test for modal footer

--- src/molecules/modal-footer/stories/modal-footer.stories.tsx
+++ src/molecules/modal-footer/stories/modal-footer.stories.tsx
@@ -1,6 +1,9 @@
+import { ComponentStory } from '@storybook/react';
+import { userEvent, within } from '@storybook/testing-library';
 import React from 'react';
 import ModalFooter, { ModalFooterProps } from '..';
 import { Button } from '../../..';
+import { expect, jest } from '@storybook/jest';
 
 export default {
   title: 'Components/Molecules/Modal Footer',
@@ -8,22 +11,41 @@ export default {
   argTypes: {},
 };
 
-export const ModalFooterComponentNoChildren = (args: ModalFooterProps) => (
-  <ModalFooter {...args} />
-);
+export const ModalFooterComponentNoChildren: ComponentStory<
+  typeof ModalFooter
+> = (args: ModalFooterProps) => <ModalFooter {...args} />;
 
+const mockedCloseAction = jest.fn();
 ModalFooterComponentNoChildren.storyName = 'No children';
 ModalFooterComponentNoChildren.args = {
   closeValue: 'Cancel',
+  dataTestId: 'footer',
+  closeAction: mockedCloseAction,
+};
+ModalFooterComponentNoChildren.play = ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  const footer = canvas.getByTestId('footer');
+  expect(footer).toBeVisible();
+
+  expect(mockedCloseAction).toHaveBeenCalledTimes(0);
+  userEvent.click(canvas.getByRole('button'));
+  expect(mockedCloseAction).toHaveBeenCalledTimes(1);
 };
 
-export const ModalFooterComponentChildren = (args: ModalFooterProps) => (
-  <ModalFooter {...args}>
-    <Button type="submit" value="save" />
-  </ModalFooter>
-);
+export const ModalFooterComponentChildren: ComponentStory<typeof ModalFooter> =
+  (args: ModalFooterProps) => (
+    <ModalFooter {...args}>
+      <Button type="submit" value="save" dataTestId="children" />
+    </ModalFooter>
+  );
 
 ModalFooterComponentChildren.storyName = 'With children';
 ModalFooterComponentChildren.args = {
   closeValue: 'Abort',
+};
+ModalFooterComponentChildren.play = ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  const children = canvas.getByTestId('children');
+  // Make sure the children have rendered
+  expect(children).toBeVisible();
 };
