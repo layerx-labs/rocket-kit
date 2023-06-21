38820a7f12cbb3118ed8825eb07707c91842e6b7
Henrique Macedo <henrique@taikai.network>
2023-06-21 11:46:40 +0100

    Bump Storybook to 7.0.22

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,10 +8,25 @@ module.exports = {
     '@storybook/addon-controls',
     '@storybook/addon-a11y',
     '@storybook/addon-links',
-    'storybook-addon-designs',
+    '@storybook/addon-mdx-gfm',
+    '@storybook/addon-styling',
+    {
+      name: '@storybook/addon-styling',
+      options: {},
+    },
   ],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
+  },
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: { fastRefresh: true },
+  },
+  docs: {
+    autodocs: true,
+  },
+  core: {
+    disableTelemetry: true, // 👈 Disables telemetry
   },
 };
